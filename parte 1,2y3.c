c2be88aad645d6aabe16586cc39c476ac2f7992b
Skarleth24102 <ram24102@uvg.edu.gt>
2026-09-20 20:01:53 -0600

    Add files via upload

---
+++ parte 1,2y3.c
@@ -0,0 +1,508 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define ftimclk 48000000
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim2;
+TIM_HandleTypeDef htim6;
+TIM_HandleTypeDef htim7;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+volatile uint8_t flagMedicion = 0;
+volatile uint8_t numFlanco = 0;
+
+volatile uint32_t flanco1 = 0;
+volatile uint32_t flanco2 = 0;
+
+uint32_t delta = 0;
+
+float frecuencia = 0;
+
+char mensaje[50];
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM2_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_TIM6_Init(void);
+static void MX_TIM7_Init(void);
+
+/* USER CODE BEGIN PFP */
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+  /* USER CODE END Init */
+
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+  /* USER CODE END SysInit */
+
+  MX_GPIO_Init();
+  MX_TIM2_Init();
+  MX_USART2_UART_Init();
+  MX_TIM6_Init();
+  MX_TIM7_Init();
+
+  /* USER CODE BEGIN 2 */
+
+  // TIM2 - Medicion de frecuencia
+  HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_1);
+
+  // TIM6 - LED rojo - periodo total 500 ms
+  HAL_TIM_Base_Start_IT(&htim6);
+
+  // TIM7 - LED verde - periodo total 2 segundos
+  HAL_TIM_Base_Start_IT(&htim7);
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+      if (flagMedicion == 1)
+      {
+          flagMedicion = 0;
+
+          if (flanco2 >= flanco1)
+          {
+              delta = flanco2 - flanco1;
+          }
+          else
+          {
+              delta = (0xFFFFFFFF - flanco1) + flanco2 + 1;
+          }
+
+          if (delta > 0)
+          {
+              frecuencia =
+                  (ftimclk / (float)(htim2.Init.Prescaler + 1))
+                  / (float)delta;
+          }
+
+          sprintf(mensaje,"Frecuencia: %lu Hz\r\n",(uint32_t)frecuencia);
+          HAL_UART_Transmit(&huart2,(uint8_t*)mensaje,strlen(mensaje),HAL_MAX_DELAY);
+
+          HAL_Delay(500);
+
+          HAL_TIM_IC_Start_IT(&htim2,TIM_CHANNEL_1);
+      }
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  __HAL_PWR_VOLTAGESCALING_CONFIG(
+      PWR_REGULATOR_VOLTAGE_SCALE3
+  );
+
+  RCC_OscInitStruct.OscillatorType =RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState =RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue =RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState =RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource =RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 96;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  RCC_ClkInitStruct.ClockType =RCC_CLOCKTYPE_HCLK |RCC_CLOCKTYPE_SYSCLK |RCC_CLOCKTYPE_PCLK1 |RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource =RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider =RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider =RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider =RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct,FLASH_LATENCY_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+  /* USER CODE BEGIN TIM2_Init 0 */
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_IC_InitTypeDef sConfigIC = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+  /* USER CODE END TIM2_Init 1 */
+
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 1;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 4294967295;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+
+  if (HAL_TIM_ConfigClockSource(&htim2,&sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  if (HAL_TIM_IC_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2,&sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
+  sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
+  sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
+  sConfigIC.ICFilter = 0;
+
+  if (HAL_TIM_IC_ConfigChannel(&htim2,&sConfigIC,TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* USER CODE BEGIN TIM2_Init 2 */
+  /* USER CODE END TIM2_Init 2 */
+}
+
+/**
+  * @brief TIM6 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM6_Init(void)
+{
+  /* USER CODE BEGIN TIM6_Init 0 */
+  /* USER CODE END TIM6_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM6_Init 1 */
+  /* USER CODE END TIM6_Init 1 */
+
+  htim6.Instance = TIM6;
+  htim6.Init.Prescaler = 47999;
+  htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim6.Init.Period = 249;
+  htim6.Init.AutoReloadPreload =
+      TIM_AUTORELOAD_PRELOAD_DISABLE;
+
+  if (HAL_TIM_Base_Init(&htim6) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  sMasterConfig.MasterOutputTrigger =TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode =TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim6,&sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* USER CODE BEGIN TIM6_Init 2 */
+  /* USER CODE END TIM6_Init 2 */
+}
+
+/**
+  * @brief TIM7 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM7_Init(void)
+{
+  /* USER CODE BEGIN TIM7_Init 0 */
+  /* USER CODE END TIM7_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM7_Init 1 */
+  /* USER CODE END TIM7_Init 1 */
+
+  htim7.Instance = TIM7;
+  htim7.Init.Prescaler = 47999;
+  htim7.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim7.Init.Period = 999;
+  htim7.Init.AutoReloadPreload =TIM_AUTORELOAD_PRELOAD_DISABLE;
+
+  if (HAL_TIM_Base_Init(&htim7) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  sMasterConfig.MasterOutputTrigger =TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode =TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim7,&sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* USER CODE BEGIN TIM7_Init 2 */
+  /* USER CODE END TIM7_Init 2 */
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+  /* USER CODE BEGIN USART2_Init 0 */
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+  /* USER CODE END USART2_Init 1 */
+
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength =UART_WORDLENGTH_8B;
+  huart2.Init.StopBits =UART_STOPBITS_1;
+  huart2.Init.Parity =UART_PARITY_NONE;
+  huart2.Init.Mode =UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl =UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling =UART_OVERSAMPLING_16;
+
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* USER CODE BEGIN USART2_Init 2 */
+  /* USER CODE END USART2_Init 2 */
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /* D7 = PA8 = VERDE
+     D8 = PA9 = ROJO */
+
+  HAL_GPIO_WritePin(GPIOA,LD2_Pin |VERDE_Pin |ROJO_Pin,GPIO_PIN_RESET);
+  /*Configure GPIO pin : B1_Pin */
+
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode =GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull =GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port,&GPIO_InitStruct);
+
+  /*Configure GPIO pins : LD2_Pin VERDE_Pin ROJO_Pin */
+
+  GPIO_InitStruct.Pin =LD2_Pin |VERDE_Pin |ROJO_Pin;
+  GPIO_InitStruct.Mode =GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull =GPIO_NOPULL;
+  GPIO_InitStruct.Speed =GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA,&GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* TIM2 - Medicion de frecuencia */
+
+void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
+{
+    if (htim->Instance == TIM2)
+    {
+        if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)
+        {
+            if (numFlanco == 0)
+            {
+                flanco1 =
+                    HAL_TIM_ReadCapturedValue(
+                        &htim2,
+                        TIM_CHANNEL_1
+                    );
+
+                numFlanco = 1;
+            }
+            else
+            {
+                flanco2 =
+                    HAL_TIM_ReadCapturedValue(
+                        &htim2,
+                        TIM_CHANNEL_1
+                    );
+
+                HAL_TIM_IC_Stop_IT(
+                    &htim2,
+                    TIM_CHANNEL_1
+                );
+
+                numFlanco = 0;
+
+                flagMedicion = 1;
+            }
+        }
+    }
+}
+
+/* TIM6 y TIM7 - Generacion de señales */
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+    /* TIM6
+       LED ROJO D8
+       250 ms encendido
+       250 ms apagado
+       Periodo total = 500 ms */
+
+    if (htim->Instance == TIM6)
+    {
+        HAL_GPIO_TogglePin(ROJO_GPIO_Port,ROJO_Pin
+        );
+    }
+
+    /* TIM7
+       LED VERDE D7
+       1 segundo encendido
+       1 segundo apagado
+       Periodo total = 2 segundos */
+
+    if (htim->Instance == TIM7)
+    {
+        HAL_GPIO_TogglePin(VERDE_GPIO_Port,VERDE_Pin
+        );
+    }
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+
+  __disable_irq();
+
+  while (1)
+  {
+  }
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef USE_FULL_ASSERT
+
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* USER CODE END 6 */
+}
+
+#endif /* USE_FULL_ASSERT */
